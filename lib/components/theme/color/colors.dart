58b8272a46d9757021b9bfa4e9619645b2d6d112
Fatih Can <ybsfatihcan@gmail.com>
2020-10-05 20:38:29 +0300

    color

---
+++ lib/components/theme/color/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+Color oneColor = Color(0xFFBF381A);
+Color secondColor = Color(0xFFEA640D);
+Color thirdColor = Color(0xFFF5A64A);
+Color fourthColor = Color(0xFFFBC57B);
+Color fifthColor = Color(0xFFFCDCA3);
+Color sixthColor = Color(0xFFFFFFFF);
