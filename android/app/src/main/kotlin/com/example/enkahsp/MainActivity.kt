7ddeeae88dbe6b99a2251ae36b32035cc52b182d
Fatih Can <ybsfatihcan@gmail.com>
2020-10-05 10:34:47 +0300

    splash view

---
+++ android/app/src/main/kotlin/com/example/enkahsp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.enkahsp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/splash_view.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
+  SplashView({Key key}) : super(key: key);
+
+  @override
+  _SplashViewState createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  String assetImage = "";
+  void initState() {
+    super.initState();
+    Future.delayed(
+        Duration(
+          seconds: 3,
+        ), () {
+      Navigator.pushNamed(context, '/home');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.asset(assetImage),
+      ),
+    );
+  }
+}
